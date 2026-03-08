bd138d3b5fb7520c1ad0ad7857662ccd25f9ea9e
Pooja <poojapoojapoojapooja110@gmail.com>
2026-03-08 16:21:00 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+apiKey: "AIzaSyAgjc3rXgx_JgUBvnTLv6c32HW4UNyu6Cw",
+authDomain: "bus-tracking-system-78e94.firebaseapp.com",
+databaseURL: "https://bus-tracking-system-78e94-default-rtdb.firebaseio.com",
+projectId: "bus-tracking-system-78e94",
+storageBucket: "bus-tracking-system-78e94.appspot.com",
+messagingSenderId: "316084412095",
+appId: "1:316084412095:web:6438bdbf0e7599b5c2f00a"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
