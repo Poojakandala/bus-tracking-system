957a6fec9c94e9c173e4a6ac5fe59cd12225f989
Pooja <poojapoojapoojapooja110@gmail.com>
2026-07-07 11:02:34 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+// Original project's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyAgjc3rXgx_JgUBvnTLv6c32HW4UNyu6Cw",
-    authDomain: "bus-tracking-system-78e94.firebaseapp.com",
-    databaseURL: "https://bus-tracking-system-78e94-default-rtdb.firebaseio.com",
-    projectId: "bus-tracking-system-78e94"
+    authDomain: "bus-tracking-system-vignan.firebaseapp.com",
+    databaseURL: "https://bus-tracking-system-vignan-default-rtdb.firebaseio.com",
+    projectId: "bus-tracking-system-vignan",
+    storageBucket: "bus-tracking-system-vignan.appspot.com",
+    messagingSenderId: "000000000000",
+    appId: "1:000000000000:web:000000000000000000000000"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
+export const auth = getAuth(app);
